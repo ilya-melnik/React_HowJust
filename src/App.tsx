9e6d3162f8779b0a688a49cfe6b08fb277111a2a
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-05-20 11:49:12 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordeon/Accordeon";
-import {Rating, ValueType} from "./components/Rating/Rating";
-import {OnnOff} from "./components/OnOff/OnOff";
-
-import {UnControlledRating} from "./components/UnControlledRating/UnControlledRating";
-import {UnControlledOnnOff} from "./components/UnControlledOnOff/OnOff";
-import {UnControlledAccordion} from "./components/UnControlledAccordeon/UnControlldAccordeon";
-import {Input} from "./stories/Input";
-
-
 
 function  App() {
-
-
-    let [ratingValue, setRatingValue] = useState<ValueType>(3)
-    let [colapsedAcordion, setColapsedAcordion] = useState<boolean>(false)
-    let [SwichOnn, setSwichOnn] = useState(false)
-
-function onChange ()  {  setSwichOnn(!SwichOnn)}
     return (
-
         <div className="App">
-            <Input/>
-            <OnnOff on={SwichOnn}
-                    ChangeStatus={onChange}/>
-
-           <UnControlledOnnOff status={setSwichOnn}/>{SwichOnn.toString()}
-
-            <Accordion collapsed={colapsedAcordion}
-                       onChange={ () => {setColapsedAcordion(!colapsedAcordion)} }
-                       titleValue={"MENU"}/>
-
-            <Rating value={ratingValue}
-                    onClick={setRatingValue}/>
-            <UnControlledAccordion titleValue={"Menu"}/>
-            <UnControlledAccordion titleValue={"Users"}/>
-            <UnControlledRating/>
-
-        </div>
+            Hello
+          </div>
     );
 }
-
-function PageTitle(props: any) {
-    return (
-        <div>{props.title}</div>
-    )
-}
-
 
 export default App;
