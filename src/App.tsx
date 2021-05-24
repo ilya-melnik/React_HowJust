15a42c9810490d169d7024637da0c831d398cc86
ilya-melnik <ilay.melnik.kr@gmail.com>
2021-05-24 12:00:48 +0300

    ReactJust  2 end

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import './App.css';
 
-function  App() {
+function App() {
     return (
         <div className="App">
-            This is APP component
+            <AppTitle />
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
-
-function  Rating() {
+function AppTitle () {
+    return <>This is APP component</>
+}
+function Rating() {
     return (
         <div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
-            <div>start</div>
+            <Star />
+            <Star />
+
         </div>
     );
 }
 
+function Star() {
+    return <div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+    </div>
+}
+
+function Accordion() {
+    return <div>
+        <AccordionTitle />
+        <AccordionBody />
+    </div>
+}
+
+
+function AccordionTitle () {
+    return <div>
+        <h3>Меню</h3>
+    </div>
+}
+
+function AccordionBody () {
+    return <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+}
 export default App;
